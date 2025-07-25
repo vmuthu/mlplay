447b2728ccc6aaf0b01df7079f9448ca781758c6
Muthu V <muthu@pmsquaresoft.com>
2025-07-25 11:36:21 +0530

    First commit of sample c# code

---
+++ MlPlayGround/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("This Program is for Snippet4AIV1\\n");
+string fullNameOfUser = null;
+string firstName = fullNameOfUser.Split(' ')[0];
+Console.WriteLine(firstName);
